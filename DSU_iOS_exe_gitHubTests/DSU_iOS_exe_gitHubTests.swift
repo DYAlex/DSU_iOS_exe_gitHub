10fc2dfe1ca5addcf21a30e47a73fd4ad8cff299
YD <dyalex@mbpr.local>
2017-04-06 03:39:55 +0300

    Initial Commit

---
+++ DSU_iOS_exe_gitHubTests/DSU_iOS_exe_gitHubTests.swift
@@ -0,0 +1,36 @@
+//
+//  DSU_iOS_exe_gitHubTests.swift
+//  DSU_iOS_exe_gitHubTests
+//
+//  Created by Yvette Demidova on 06/04/2017.
+//  Copyright © 2017 YD. All rights reserved.
+//
+
+import XCTest
+@testable import DSU_iOS_exe_gitHub
+
+class DSU_iOS_exe_gitHubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
